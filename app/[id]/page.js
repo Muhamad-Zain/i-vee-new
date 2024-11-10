8b8b41e08fd86f25d9bda2d826dbbb3d154a9a3b
Muhamad_Zain <zainmuhamad17@gmail.com>
2024-11-10 21:49:09 +0700

    update

--- app/[id]/page.js
+++ app/[id]/page.js
@@ -1,7 +1,7 @@
 'use client'
 import App from "@/components/app/page"
 import { useParams } from "next/navigation"
-import Rsvp from "../rsvp/page"
+import Rsvp from "../rsvp/[id]/page"
 // import { useRouter } from "next/router"
 
 export default function id() {
